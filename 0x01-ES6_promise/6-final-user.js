aec8034613ff4720a5adc048a77192b0d0454096
Reda <muhammadredabh@gmail.com>
2023-03-22 11:21:05 +0000

    return array fixed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,9 +4,12 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then(
-      [(results) => results.map((el) => ({
-        status: el.status, value: el.value ? el.value : el.value,
-      })),
-      ],
+      (results) => {
+        const result = [];
+        results.forEach((el) => {
+          result.push({ status: el.status, value: el.value ? el.value : el.reason });
+        });
+        return result;
+      },
     );
 }
