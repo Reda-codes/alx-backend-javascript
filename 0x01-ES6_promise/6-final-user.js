d4085d44aa198968d2048b351786baf4779a150a
Reda <muhammadredabh@gmail.com>
2023-03-22 10:37:00 +0000

    replaces promise all with allseateled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => values)
-    .catch((err) => err);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results);
 }
