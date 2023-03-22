84a542255227110a3847388124beff5d08532cb1
Reda <muhammadredabh@gmail.com>
2023-03-22 10:03:49 +0000

    Fixed 2ed & 3rd challenges

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(
-    (value) => {
-      console.log('Got a response from the API');
-      return value;
-    },
-    (error) => {
-      console.log('Got a response from the API');
-      return error;
-    },
-  );
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  uploadPhoto().then((value) => process.stdout.write(value.body));
-  createUser().then((value) => console.log(` ${value.firstName} ${value.lastName}`));
+  Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  });
 }
