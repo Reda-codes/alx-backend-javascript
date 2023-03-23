d155563817067c055197dc79af91c62a51cef0e1
Reda <muhammadredabh@gmail.com>
2023-03-23 16:18:06 +0000

    7. Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,30 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') throw new Error('Name must be a string');
+    if (typeof code !== 'string') throw new Error('Code must be a string');
+    this._name = name;
+    this._code = code;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw new Error('Name must be a string');
+    this._name = name;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') throw new Error('Code must be a string');
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return (this._code);
+  }
+}
