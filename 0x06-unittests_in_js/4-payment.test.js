e57cd69bf916e70b466ae90855dd23bbf0590c05
Reda <muhammadredabh@gmail.com>
2023-05-18 23:05:14 +0100

    4. Stubs

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,16 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('Utils called withing function & console.log is correct', () => {
+    const calculateNumberSpy = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleLogSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
+    calculateNumberSpy.restore();
+    consoleLogSpy.restore();
+  });
+});
