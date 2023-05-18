b8a6614997303c78d966149a47b0a1affc4c485b
Reda <muhammadredabh@gmail.com>
2023-05-18 15:03:53 +0100

    fix

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,19 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable jest/prefer-expect-assertions */
 /* eslint-disable jest/expect-expect */
 const { expect } = require('chai');
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', () => {
+describe('type as SUM', () => {
   it('type as SUM', () => {
     expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-  });
-  it('type as SUBTRACT', () => {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-  });
-  it('type as DIVIDE', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-  });
-  it('type as DIVIDE but with b = 0', () => {
-    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
   });
 });
