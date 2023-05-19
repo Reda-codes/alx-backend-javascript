bcece7cd9afdca5e686279ae7c2f8f7df1924122
Reda <muhammadredabh@gmail.com>
2023-05-19 17:38:00 +0100

    fix Error case

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,20 +1,15 @@
 function calculateNumber(type, a, b) {
-  switch (type) {
-    case 'SUM':
-      return (Math.round(a) + Math.round(b));
-
-    case 'SUBTRACT':
-      return (Math.round(a) - Math.round(b));
-
-    case 'DIVIDE':
-      if (b === 0) {
-        return 'Error';
-      }
-      return (Math.round(a) / Math.round(b));
-
-    default:
-      return 'Error: No Type was passed';
+  if (type === 'SUM') {
+    return (Math.round(a) + Math.round(b));
+  } if (type === 'SUBTRACT') {
+    return (Math.round(a) - Math.round(b));
+  } if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    }
+    return (Math.round(a) / Math.round(b));
   }
+  return 'Error';
 }
 
 module.exports = calculateNumber;
