4dee0cdc4662c0067e39a75319dc35c55508b987
Reda <muhammadredabh@gmail.com>
2023-05-19 17:43:42 +0100

    fixed utils function

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,22 +1,18 @@
 const Utils = {
-    calculateNumber(type, a, b) {
-        switch (type) {
-            case 'SUM':
-            return (Math.round(a) + Math.round(b));
-
-            case 'SUBTRACT':
-            return (Math.round(a) - Math.round(b));
-
-            case 'DIVIDE':
-            if (b === 0) {
-                return 'Error';
-            }
-            return (Math.round(a) / Math.round(b));
-
-            default:
-            return 'Error: No Type was passed';
-        }
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') {
+      return (Math.round(a) + Math.round(b));
+    } if (type === 'SUBTRACT') {
+      return (Math.round(a) - Math.round(b));
+    } if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return (Math.round(a) / Math.round(b));
     }
+    return 'Error';
+}
+
 }
 
 module.exports = Utils;
