de304ff81497dacd3484725569d0653f98fcd4f1
Reda <muhammadredabh@gmail.com>
2023-05-19 17:27:47 +0100

    fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-restricted-globals */
-/* eslint-disable jest/prefer-expect-assertions */
-/* eslint-disable jest/expect-expect */
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
@@ -13,6 +10,9 @@ describe('calculateNumber', () => {
   });
   it('rounddown float & roundup float', () => {
     assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('integer & a roundup float', () => {
+    assert.equal(calculateNumber(3.6, 1), 5);
   });
   it('two roundup float numbers', () => {
     assert.equal(calculateNumber(1.5, 3.7), 6);
